41b9e131880dbc2ef6fdcb5a56bf24eed6732f43
Ozair-Hassan <ozair.hassan123@gmail.com>
2023-11-08 17:15:45 +0500

    scroll updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,24 @@ import HomePage from './Pages/HomePage'
 import CoinPage from './Pages/CoinPage'
 
 const App = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      document.body.classList.add('scrolling')
+
+      // Remove 'scrolling' class after 1 second of no scrolling
+      clearTimeout(document.body.scrollTimeout)
+      document.body.scrollTimeout = setTimeout(() => {
+        document.body.classList.remove('scrolling')
+      }, 1000)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      clearTimeout(document.body.scrollTimeout)
+    }
+  }, [])
   return (
     <>
       <BrowserRouter>
